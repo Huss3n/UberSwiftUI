26607f7ab686465ff88d87b9b18ba8a740c7d63f
Hussein <aisackhussein@gmail.com>
2024-09-10 13:15:38 +0300

    Add Firebase Dependency

--- UberSwiftUI/App/UberSwiftUIApp.swift
+++ UberSwiftUI/App/UberSwiftUIApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+print("Firebase Configured")
+    return true
+  }
+}
 
 @main
 struct UberSwiftUIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     @StateObject var locationSearchVM = LocationSearchVM()
     
     var body: some Scene {
